b89c5aa1360971ed774fdc415717b095630af4f8
Laura <laura.leisberg@gmail.com>
2019-11-10 01:47:12 +0200

    rfcrfc

--- leisberg/src/components/Description.vue
+++ leisberg/src/components/Description.vue
@@ -13,7 +13,7 @@
     }
 </script>
 
-<!-- Add "scoped"c attribute rfrto lizszsmitrf CSS to this component only -->
+<!-- Add "scoped"c attgftrribute rfrto lizszsmitrf CSS to this component only -->
 <style scoped>
   .desc {
     background: white;
