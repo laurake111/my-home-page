03a2fd8b5975fe8cb92e4eeef3335bd6b3eae6de
Laura <laura.leisberg@gmail.com>
2019-11-10 01:34:14 +0200

    ok

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <HelloWorld/>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/Description.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
+
+<style>
+    body {
+        background-image: url("assets/taust.jpg");
+        background-size: cover;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/Description.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="desc">
+        <h1>Welcome!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped"c attribute rfrto lizszsmit CSS to this component only -->
+<style scoped>
+    .desc {
+        background: white;
+        border-style: groove;
+        width: 500px;
+        height: 400px;
+        float: left;
+        margin-left: 50px;
+    }
+</style>
